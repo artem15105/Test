f9fa02ebb4053ba7a3f509e8691d9ed0628bc3bc
artem15105 <artem.kulabuxov@yandex.ru>
2021-07-11 01:24:13 +0300

    artem3

---
+++ Assets/Scipts/Template/AdsCore.cs
@@ -0,0 +1,91 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Advertisements;
+
+public class AdsCore : MonoBehaviour, IUnityAdsListener
+{
+    [SerializeField] private bool _testMode = true;
+
+    private string _gameId = "4211852"; //ваш game id
+
+    private string _video = "Interstitial_Android";
+    private string _rewardedVideo = "Rewarded_Android";
+    private string _banner = "Banner_Android";
+
+    void Start() // инициализаци€ сервисов
+    {
+        Advertisement.AddListener(this);
+        Advertisement.Initialize(_gameId, _testMode);
+
+        
+
+        #region Banner
+
+        StartCoroutine(ShowBannerWhenInitialized());
+        Advertisement.Banner.SetPosition(BannerPosition.TOP_CENTER);
+
+        #endregion
+    }
+    public void Update()
+    {
+        
+    }
+
+    public static void ShowAdsVideo(string placementId) //инициализаци€ рекламы по типу
+    {
+        if (Advertisement.IsReady())
+        {
+            Advertisement.Show(placementId);
+        }
+        else
+        {
+            Debug.Log("Advertisement not ready!");
+        }
+    }
+
+    IEnumerator ShowBannerWhenInitialized()
+    {
+        while (!Advertisement.isInitialized)
+        {
+            yield return new WaitForSeconds(0.5f);
+        }
+        Advertisement.Banner.Show(_banner);
+    }
+
+    public void OnUnityAdsReady(string placementId)
+    {
+        if (placementId == _rewardedVideo)
+        {
+               //действи€, если реклама доступна
+        }
+    }
+
+    public void OnUnityAdsDidError(string message)
+    {
+        //ошибка рекламы
+    }
+
+    public void OnUnityAdsDidStart(string placementId)
+    {
+        // только запустили рекламу
+    }
+
+    public void OnUnityAdsDidFinish(string placementId, ShowResult showResult) //обработка рекламы (тут определе€ем вознаграждение)
+    {
+        if (showResult == ShowResult.Finished)
+        {
+            Debug.Log("+1");
+            //действи€, если пользователь посмотрел рекламу до конца
+        }
+        else if (showResult == ShowResult.Skipped)
+        {
+            Debug.Log("„јѕќЋ№");
+            //действи€, если пользователь пропустил рекламу + „јѕќЋ№
+        }
+        else if (showResult == ShowResult.Failed)
+        {
+            //действи€ при ошибке
+        }
+    }
+}
+
--- Assets/Scipts/Test/Control.cs
+++ Assets/Scipts/Test/Control.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using UnityEngine.Advertisements;
 
 public class Control : MonoBehaviour
 {
@@ -14,10 +15,20 @@ public class Control : MonoBehaviour
     private int number = 0;
     public List<GameObject> questions = new List<GameObject>();
 
+
+    public void Start()
+    {
+        if (Advertisement.isSupported)
+            Advertisement.Initialize("4211852", false);
+        if (Advertisement.IsReady())
+            Advertisement.Show("video");
+    }
     public void next()
     {
         if(questions.Count != number + 1)
         {
+            if (number + 1 == 7)
+                AdsCore.ShowAdsVideo("Interstitial_Android");
             questions[number].SetActive(false);
             questions[number + 1].SetActive(true);
             number++;
